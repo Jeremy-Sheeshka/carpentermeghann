21c6dfa8718f3cf5b07590654effeaf471dca7b6
Jeremy Sheeshka <Jsheeshka@gmail.com>
2025-01-11 13:40:44 -0800

    test

--- server.js
+++ server.js
@@ -1,57 +1,50 @@
-
 const express = require('express');
 const app = express();
-
-const nodemailer = require("nodemailer");
-
+const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware//
+// Middleware
+app.use(cors());
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/contact.html')
-})
-
-app.post('/', (req, res)=>{
-  console.log(req.body)
-    
-
-// create a .env file in your project and paste your password and email there as a variables and use those variables on your contact form 
-    
-//app password is from gmail "app password" "app name"
-const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        port: '465',
-        auth: {
-            user: 'teachersheeshka@gmail.com',
-            pass: 'rifw laoi wmqq iwbr'
-        }
-    });
-
-    const mailOptions = {
-           from: req.body.email, 
-           //WHERE IS THE EMAIL GOING??
-           to: 'jsheeshka@gmail.com',
-           subject: `Message from ${req.body.email}: ${req.body.subject}`,
-           text: req.body.message
-        };
-
-        transporter.sendMail(mailOptions, (error, info)=>{
-                if(error){
-                      console.log(error);
-                      res.send('error');
-                  }else{ 
-                      console.log('Email sent: ' + info.response);
-                      res.send('success')
-                  }
-  
-  
-    });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-}) 
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: '465',
+    auth: {
+      user: 'teachersheeshka@gmail.com',
+      pass: 'rifw laoi wmqq iwbr'
+    }
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: 'jsheeshka@gmail.com',
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
